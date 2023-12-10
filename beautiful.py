6776e8a7c3e05c2972522f46998beda23cdcc053
Wannero <silver-79@live.fr>
2023-12-09 18:55:00 -0800

    anasfalih & mounirabbad

---
+++ beautiful.py
@@ -0,0 +1,14 @@
+# This is a beautiful Python code example
+
+def greet(name):
+    """
+    This function greets the person passed in as a parameter.
+    """
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    # Get the user's name
+    user_name = input("Enter your name: ")
+
+    # Greet the user
+    greet(user_name)
